290dad4da947312edf7a7ab0c07be92ff6add171
Felicity520666 <yyan520666@gmail.com>
2026-07-28 11:38:45 -0400

    assets

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Collection Style Game
+## About This Game
+
+
+
+## How to Play
+
+
+
+## Features
+
+
+
+## Attributions
+- Background image: [Kpop-Demon-Hunters.png](assets/Kpop-Demon-Hunters.png)
+
+## Created By
+
+
 
---
+++ script.js
@@ -0,0 +1,147 @@
+const WINNING_SCORE = 5;
+
+let catcher, fallingObject;
+let score = 0;
+let gameStatus = "playing";
+let backgroundImg;
+
+/* PRELOADS FILES */
+function preload () {
+    backgroundImg = loadImage("assets/Kpop-Demon-Hunters.png");
+}
+
+function setup() {
+    const canvas = createCanvas(400, 400);
+    const gameContainer = document.getElementById("game");
+
+    if (gameContainer) {
+        canvas.parent(gameContainer);
+    }
+
+    catcher = new Sprite(200, 380, 100, 20, "k");
+    catcher.color = color(95, 158, 160);
+
+    fallingObject = new Sprite(100, 0, 10);
+    fallingObject.color = color(0, 128, 128);
+    fallingObject.vel.y = 2;
+}
+
+function draw() {
+    background(224);
+
+    if (gameStatus === "won") {
+        youWin();
+        return;
+    }
+
+    if (gameStatus === "lost") {
+        youLose();
+        return;
+    }
+
+    textAlign(LEFT, TOP);
+    fill(0);
+    textSize(12);
+    text(
+        "Move the catcher with the left and right arrow keys to catch the falling objects.",
+        width - 100,
+        20,
+        90
+    );
+
+    if (fallingObject.y >= height) {
+        score -= 1;
+
+        if (score < 0) {
+            score = 0;
+            endGame("lost");
+            return;
+        }
+
+        resetFallingObject();
+    }
+
+    if (kb.pressing("left")) {
+        catcher.vel.x = -3;
+    } else if (kb.pressing("right")) {
+        catcher.vel.x = 3;
+    } else {
+        catcher.vel.x = 0;
+    }
+
+    if (catcher.x < 50) {
+        catcher.x = 50;
+    } else if (catcher.x > 350) {
+        catcher.x = 350;
+    }
+
+    if (fallingObject.collides(catcher)) {
+        score += 1;
+
+        if (score >= WINNING_SCORE) {
+            endGame("won");
+            return;
+        }
+
+        resetFallingObject();
+    }
+
+    fill(0, 128, 128);
+    textSize(20);
+    text("Score = " + score, 10, 30);
+}
+
+function endGame(result) {
+    gameStatus = result;
+    catcher.vel.x = 0;
+    fallingObject.vel.y = 0;
+
+    catcher.x = 2000;
+    catcher.y = 2000;
+    fallingObject.x = 2000;
+    fallingObject.y = 2000;
+}
+
+function youWin() {
+    drawEndScreen("Bravo! You Win", "Click to play again");
+
+    if (mouseIsPressed) {
+        restart();
+    }
+}
+
+function youLose() {
+    drawEndScreen("Oh No! You Lose", "Click to try again");
+
+    if (mouseIsPressed) {
+        restart();
+    }
+}
+
+function drawEndScreen(message, instruction) {
+    textAlign(CENTER, CENTER);
+    fill(0, 128, 128);
+    textSize(30);
+    text(message, width / 2, height / 2 - 15);
+    textSize(16);
+    text(instruction, width / 2, height / 2 + 25);
+}
+
+function restart() {
+    score = 0;
+    gameStatus = "playing";
+
+    catcher.x = 200;
+    catcher.y = 380;
+    catcher.visible = true;
+
+    fallingObject.visible = true;
+    resetFallingObject();
+}
+
+function resetFallingObject() {
+    fallingObject.y = 0;
+    fallingObject.x = random(width);
+    fallingObject.vel.y = random(1, 5);
+    fallingObject.direction = "down";
+}
